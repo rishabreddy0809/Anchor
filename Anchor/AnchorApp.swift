d09f681601fba0d852275ea1f7c1c776c26e0c0f
rishabreddy0809 <rishabreddy0809@gmail.com>
2026-07-11 12:40:54 -0400

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by Rishab Reddy Paili on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
